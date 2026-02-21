5390d6b0ac7c059071aeec0b678524c88ae5417f
AlDer <halfordderrick@gmail.com>
2026-02-21 12:42:17 +0530

    Added Routers

---
+++ routes/hostRouter.js
@@ -0,0 +1,8 @@
+// External Module
+const express = require("express");
+const hostRouter = express.Router();
+
+// Local Module
+const hostController = require("../controllers/hostController");
+
+module.exports = hostRouter;
---
+++ routes/storeRouter.js
@@ -0,0 +1,8 @@
+// External Module
+const express = require("express");
+const storeRouter = express.Router();
+
+// Local Module
+const homesController = require("../controllers/storeController");
+
+module.exports = storeRouter;
